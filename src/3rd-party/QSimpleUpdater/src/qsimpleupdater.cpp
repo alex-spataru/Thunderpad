7ef82777c8caef0800497c8579dc5315a34d1422
Alex Spataru <alex.racotta@gmail.com>
2014-11-05 15:26:40 -0600

    Updated QSimpleUpdater

--- src/3rd-party/QSimpleUpdater/src/qsimpleupdater.cpp
+++ src/3rd-party/QSimpleUpdater/src/qsimpleupdater.cpp
@@ -8,166 +8,163 @@
 
 #include "qsimpleupdater.h"
 
-QSimpleUpdater::QSimpleUpdater (QObject *parent) : QObject (parent)
+QSimpleUpdater::QSimpleUpdater(QObject *parent) : QObject(parent) 
 {
     m_new_version_available = false;
 }
 
-QString QSimpleUpdater::changeLog()
-{
+
+QString QSimpleUpdater::changeLog() {
     // Return the contents of the downloaded changelog
-    if (!m_changelog.isEmpty())
+    if (!m_changelog.isEmpty()) {
         return m_changelog;
+    }
 
     // If the changelog is empty, we issue a warning message in
     // the console and return an empty string
-    else
-    {
+    else {
         qWarning() << "QSimpleUpdater: change log is empty,"
                    << "did you call setChangelogUrl() and checkForUpdates()?";
         return NULL;
     }
 }
 
-void QSimpleUpdater::checkForUpdates()
-{
+void QSimpleUpdater::checkForUpdates() {
     // Only check for updates if we know which file should we download
-    if (!m_reference_url.isEmpty())
-    {
+    if (!m_reference_url.isEmpty()) {
 
         // Create a new network access manager, which allows us to
         // download our desired file
-        QNetworkAccessManager *_manager = new QNetworkAccessManager (this);
+        QNetworkAccessManager *_manager = new QNetworkAccessManager(this);
 
         // Compare the downloaded application version with the installed
         // version when the download is finished
-        connect (_manager, SIGNAL (finished (QNetworkReply *)),
-                 this, SLOT (checkDownloadedVersion (QNetworkReply *)));
+        connect(_manager, SIGNAL(finished(QNetworkReply*)),
+                this, SLOT(checkDownloadedVersion(QNetworkReply*)));
 
         // Ignore any possible SSL errors
-        connect (_manager, SIGNAL (sslErrors (QNetworkReply *, QList<QSslError>)),
-                 this, SLOT (ignoreSslErrors (QNetworkReply *, QList<QSslError>)));
+        connect(_manager, SIGNAL(sslErrors(QNetworkReply*,QList<QSslError>)),
+                this, SLOT(ignoreSslErrors(QNetworkReply*,QList<QSslError>)));
 
         // Finally, download the file
         _manager->get (QNetworkRequest (m_reference_url));
     }
 
     // Issue a warning message in the case that the reference URL is empty...
-    else
+    else {
         qWarning() << "QSimpleUpdater: Invalid reference URL";
+    }
 }
 
-QString QSimpleUpdater::latestVersion()
-{
+QString QSimpleUpdater::latestVersion() {
     // Return the application version referenced by the string
     // that we downloaded
-    if (!m_latest_version.isEmpty())
+    if (!m_latest_version.isEmpty()) {
         return m_latest_version;
+    }
 
     // Issue a warning message in the case that the downloaded
     // application version string is empty
-    else
-    {
+    else {
         qWarning() << "QSimpleUpdater: latest version is empty,"
                    << "did you call checkForUpdates() and setReferenceUrl()?";
         return NULL;
     }
 }
 
-QString QSimpleUpdater::installedVersion()
-{
+QString QSimpleUpdater::installedVersion() {
     // Return the string issued by the user in the setApplicationVersion() function
-    if (!m_installed_version.isEmpty())
+    if (!m_installed_version.isEmpty()) {
         return m_installed_version;
+    }
 
     // Issue a warning message in the case that the installed application
     // version is empty
-    else
-    {
+    else {
         qWarning() << "QSimpleUpdater: installed version is empty,"
                    << "did you call setApplicationVersion()?";
         return NULL;
     }
 }
 
-void QSimpleUpdater::downloadLatestVersion()
-{
+void QSimpleUpdater::downloadLatestVersion() {
     // Open the download URL in a web browser
-    if (!m_download_url.isEmpty())
-        QDesktopServices::openUrl (m_download_url);
+    if (!m_download_url.isEmpty()) {
+        QDesktopServices::openUrl(m_download_url);
+    }
 
     // The m_download_url is empty, so we issue another warning message
-    else
-    {
+    else {
         qWarning() << "QSimpleUpdater: cannot download latest version,"
                    << "did you call setDownloadUrl() and checkForUpdates()?";
     }
 }
 
-bool QSimpleUpdater::newerVersionAvailable()
-{
+bool QSimpleUpdater::newerVersionAvailable() {
     return m_new_version_available;
 }
 
-void QSimpleUpdater::setDownloadUrl (const QString url)
-{
+void QSimpleUpdater::setDownloadUrl(const QString url) {
     // Change the download URL if the issued URL is valid
-    if (!url.isEmpty())
-        m_download_url.setUrl (url);
+    if (!url.isEmpty()) {
+        m_download_url.setUrl(url);
+    }
 
     // The issued URL is ilegal, so we warn the user
-    else
+    else {
         qWarning() << "QSimpleUpdater: input URL cannot be empty!";
+    }
 }
 
-void QSimpleUpdater::setReferenceUrl (const QString url)
-{
+void QSimpleUpdater::setReferenceUrl(const QString url) {
     // Change the reference URL if the issued URL is valid
-    if (!url.isEmpty())
-        m_reference_url.setUrl (url);
+    if (!url.isEmpty()) {
+        m_reference_url.setUrl(url);
+    }
 
     // The issued URL is ilegal, so we warn the user
-    else
+    else {
         qWarning() << "QSimpleUpdater: input URL cannot be empty!";
+    }
 }
 
-void QSimpleUpdater::setChangelogUrl (const QString url)
-{
+void QSimpleUpdater::setChangelogUrl(const QString url) {
     // Change the changelog URL if the issued URL is valid
-    if (!url.isEmpty())
-        m_changelog_url.setUrl (url);
+    if (!url.isEmpty()) {
+        m_changelog_url.setUrl(url);
+    }
 
     // The issued URL is ilegal, so we warn the user
-    else
+    else {
         qWarning() << "QSimpleUpdater: input URL cannot be empty!";
+    }
 }
 
-void QSimpleUpdater::setApplicationVersion (const QString version)
-{
+void QSimpleUpdater::setApplicationVersion(const QString version) {
     // Change the installed application version if the issued string is valid
-    if (!version.isEmpty())
+    if (!version.isEmpty()) {
         m_installed_version = version;
+    }
 
     // The application version cannot be empty, so we warn the user
-    else
+    else {
         qWarning() << "QSimpleUpdater: input string cannot be empty!";
+    }
 }
 
 
-void QSimpleUpdater::checkDownloadedVersion (QNetworkReply *reply)
-{
+void QSimpleUpdater::checkDownloadedVersion(QNetworkReply *reply) {
     bool _new_update = false;
 
     // Read the reply from the server and transform it
     // to a QString
     QString _reply = QString::fromUtf8 (reply->readAll());
-    _reply.replace (" ", "");
-    _reply.replace ("\n", "");
+    _reply.replace(" ", "");
+    _reply.replace("\n", "");
 
     // If the reply from the server is not empty, compare
     // the downloaded version with the installed version
-    if (!_reply.isEmpty() && _reply.contains ("."))
-    {
+    if (!_reply.isEmpty() && _reply.contains(".")) {
 
         // Replace the latest version string with the downloaded string
         m_latest_version = _reply;
@@ -184,20 +181,17 @@ void QSimpleUpdater::checkDownloadedVersion (QNetworkReply *reply)
         QStringList _installed = m_installed_version.split (".");
 
         // Compare the major, minor, build, etc. numbers
-        for (int i = 0; i <= _download.count() - 1; ++i)
-        {
+        for (int i = 0; i <= _download.count() - 1; ++i) {
 
             // Make sure that the number that we are goind to compare
             // exists in both strings, for example, we will not compare
             // 1.2.3 and 1.2.3.1 because we would crash the program
-            if (_download.count() - 1 >= i && _installed.count() - 1 >= i)
-            {
+            if (_download.count() - 1 >= i && _installed.count() - 1 >= i) {
 
                 // The downloaded number is greater than the installed number
                 // in question. So there's a newer version of the application
                 // available.
-                if (_download.at (i) > _installed.at (i))
-                {
+                if (_download.at (i) > _installed.at (i)) {
                     _new_update = true;
                     break;
                 }
@@ -208,10 +202,8 @@ void QSimpleUpdater::checkDownloadedVersion (QNetworkReply *reply)
             // version. For example, 1.2.3 is smaller than 1.2.3.1...
             // Also, we will only reach this code when we finish comparing
             // the "3" in both the downloaded and the installed version.
-            else
-            {
-                if (_installed.count() < _download.count())
-                {
+            else {
+                if (_installed.count() < _download.count()) {
                     _new_update = true;
                     break;
                 }
@@ -230,47 +222,45 @@ void QSimpleUpdater::checkDownloadedVersion (QNetworkReply *reply)
     // there's a newer version available.
     // Note that the processDownloadedChangeLog() function will
     // notify our parent that we have finished checking for updates.
-    if (!m_changelog_url.isEmpty() && newerVersionAvailable())
-    {
-        QNetworkAccessManager *_manager = new QNetworkAccessManager (this);
+    if (!m_changelog_url.isEmpty() && newerVersionAvailable()) {
+        QNetworkAccessManager *_manager = new QNetworkAccessManager(this);
 
-        connect (_manager, SIGNAL (finished (QNetworkReply *)),
-                 this, SLOT (processDownloadedChangelog (QNetworkReply *)));
+        connect(_manager, SIGNAL(finished(QNetworkReply*)),
+                this, SLOT(processDownloadedChangelog(QNetworkReply*)));
 
-        connect (_manager, SIGNAL (sslErrors (QNetworkReply *, QList<QSslError>)),
-                 this, SLOT (ignoreSslErrors (QNetworkReply *, QList<QSslError>)));
+        connect(_manager, SIGNAL(sslErrors(QNetworkReply*,QList<QSslError>)),
+                this, SLOT(ignoreSslErrors(QNetworkReply*,QList<QSslError>)));
 
         _manager->get (QNetworkRequest (m_changelog_url));
     }
 
     // We did not download the changelog, so we notify our parent
     // that we have finished checking for updates
-    else
+    else {
         emit checkingFinished();
+    }
 }
 
-void QSimpleUpdater::processDownloadedChangelog (QNetworkReply *reply)
-{
+void QSimpleUpdater::processDownloadedChangelog(QNetworkReply *reply) {
     // Read the downloaded file and transform it to a QString
-    QString _reply = QString::fromUtf8 (reply->readAll());
+    QString _reply = QString::fromUtf8(reply->readAll());
 
     // Change the changelog string and notify our
     // parent that the changelog was downlaoded
-    if (!_reply.isEmpty())
-    {
+    if (!_reply.isEmpty()) {
         m_changelog = _reply;
         emit changelogDownloadFinished();
     }
 
     // Issue a warning in the case that the changelog is empty
-    else
+    else {
         qWarning() << "QSimpleUpdater: downloaded change log is empty!";
+    }
 
     // Tell our parent that we are done checking for updates
     emit checkingFinished();
 }
 
-void QSimpleUpdater::ignoreSslErrors (QNetworkReply *reply, QList<QSslError> error)
-{
+void QSimpleUpdater::ignoreSslErrors (QNetworkReply *reply, QList<QSslError> error) {
     reply->ignoreSslErrors (error);
 }
