e4c9ce49ea0805c008f8e8acee0bf516bed0e23f
Alex Spataru <alex.racotta@gmail.com>
2014-12-16 00:30:46 -0600

    More work

---
+++ src/editor/spell-checker.h
@@ -0,0 +1,32 @@
+//
+//  This file is part of Thunderpad
+//
+//  Copyright (c) 2013-2014 Alex Spataru <alex.racotta@gmail.com>
+//  Please check the license.txt file for more information.
+//
+
+#include <QFile>
+#include <QDebug>
+#include <QTextCodec>
+#include <QStringList>
+#include <QTextStream>
+
+#include "hunspell.h"
+#include "hunspell.hxx"
+
+class SpellChecker {
+
+public:
+    SpellChecker(const QString &dictionaryPath);
+    ~SpellChecker();
+
+    bool spell(const QString &word);
+    QStringList suggest(const QString &word);
+    void ignoreWord(const QString &word);
+
+private:
+    void put_word(const QString &word);
+    Hunspell *_hunspell;
+    QString _encoding;
+    QTextCodec *_codec;
+};
