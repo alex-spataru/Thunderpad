897a46ddbac0895b8dedea8a22c512348de6284a
Alex Spataru <alex.racotta@gmail.com>
2014-12-29 22:44:21 -0700

    Minor code improvements
    Fixed some headers duplication issues during compilation

--- src/editor/syntax-highlighter.h
+++ src/editor/syntax-highlighter.h
@@ -1,8 +1,22 @@
 //
 //  This file is part of Thunderpad
 //
-//  Copyright (c) 2013-2014 Alex Spataru <alex.racotta@gmail.com>
-//  Please check the license.txt file for more information.
+//  Copyright (c) 2013-2014 Alex Spataru <alex_spataru@outlook.com>
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111-1301
+//  USA
 //
 
 #ifndef SYNTAX_HIGHLIGHTER_H
--- src/window/toolbar.cpp
+++ src/window/toolbar.cpp
@@ -1,8 +1,22 @@
 //
 //  This file is part of Thunderpad
 //
-//  Copyright (c) 2013-2014 Alex Spataru <alex.racotta@gmail.com>
-//  Please check the license.txt file for more information.
+//  Copyright (c) 2013-2014 Alex Spataru <alex_spataru@outlook.com>
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111-1301
+//  USA
 //
 
 #include "toolbar.h"
@@ -81,7 +95,7 @@ void ToolBar::setToolbarText(bool enabled) {
     // Show the text and set the size of the toolbar
     if (enabled) {
         if (m_large_icons)
-            _toolbar_size = 44;
+            _toolbar_size = 48;
         else
             _toolbar_size = MAC_OS_X ? 36 : 48;
 
