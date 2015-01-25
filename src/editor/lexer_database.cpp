6a3b92c7aebb861a71357aae570f1d5365b4e390
Alex Spataru <alex.racotta@gmail.com>
2015-01-24 23:01:41 -0600

    Make lexer_database.cpp compilable in Travis
    - Remove lexer includes for the moment

--- src/editor/lexer_database.cpp
+++ src/editor/lexer_database.cpp
@@ -19,44 +19,8 @@
 //  USA
 //
 
-#include <Qsci/qscilexeravs.h>
-#include <Qsci/qscilexerbash.h>
-#include <Qsci/qscilexerbatch.h>
-#include <Qsci/qscilexercmake.h>
-#include <Qsci/qscilexercoffeescript.h>
-#include <Qsci/qscilexercpp.h>
-#include <Qsci/qscilexercsharp.h>
-#include <Qsci/qscilexercss.h>
-#include <Qsci/qscilexerd.h>
-#include <Qsci/qscilexerdiff.h>
-#include <Qsci/qscilexerfortran.h>
-#include <Qsci/qscilexerfortran77.h>
-#include <Qsci/qscilexerhtml.h>
-#include <Qsci/qscilexeridl.h>
-#include <Qsci/qscilexerjava.h>
-#include <Qsci/qscilexerjavascript.h>
-#include <Qsci/qscilexerlua.h>
-#include <Qsci/qscilexermakefile.h>
-#include <Qsci/qscilexermatlab.h>
-#include <Qsci/qscilexeroctave.h>
-#include <Qsci/qscilexerpascal.h>
-#include <Qsci/qscilexerperl.h>
-#include <Qsci/qscilexerpo.h>
-#include <Qsci/qscilexerpostscript.h>
-#include <Qsci/qscilexerpov.h>
-#include <Qsci/qscilexerpython.h>
-#include <Qsci/qscilexerruby.h>
-#include <Qsci/qscilexerspice.h>
-#include <Qsci/qscilexersql.h>
-#include <Qsci/qscilexertcl.h>
-#include <Qsci/qscilexertex.h>
-#include <Qsci/qscilexerverilog.h>
-#include <Qsci/qscilexervhdl.h>
-#include <Qsci/qscilexerxml.h>
-#include <Qsci/qscilexeryaml.h>
-#include "lexers/plain_text.h"
-
 #include "lexer_database.h"
+#include "lexers/plain_text.h"
 
 /*!
  * \class LexerDatabase
